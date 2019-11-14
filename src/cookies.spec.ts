a1eb0390b875b96fc7c17fe488e0ea6b4d590855
Zach Bloomquist <github@chary.us>
2019-11-14 14:25:59 -0500

    add test for setting cookies and ending up on a different domain

--- src/cookies.spec.ts
+++ src/cookies.spec.ts
@@ -27,38 +27,43 @@ describe('Cookies', () => {
   let server: Destroyable<Server>
   let app: express.Application
 
-  it('sets cookies on a long list of redirects', async () => {
-    const browser = await launchBrowser()
-    const page = await browser.newPage()
+  ([
+    ['original domain', 8],
+    ['different domain', 7]
+  ]).forEach(([ending, N]) => {
+    it(`sets cookies on a long list of redirects, ending with ${ending}`, async () => {
+      const browser = await launchBrowser()
+      const page = await browser.newPage()
 
-    app = express()
-    app.use(morgan('common'))
+      app = express()
+      app.use(morgan('common'))
 
-    const cascadingHandler: express.Handler = function(req, res) {
-      var a, b, n;
-      n = Number(req.query.n);
-      // alternates between domains
-      a = req.query.a;
-      b = req.query.b;
-      res.header("Set-Cookie", [`namefoo${n}=valfoo${n}`, `namebar${n}=valbar${n}`]);
-      if (n > 0) {
-        res.redirect(`${a}/setCascadingCookies?n=${n - 1}&a=${b}&b=${a}`);
+      const cascadingHandler: express.Handler = function(req, res) {
+        var a, b, n;
+        n = Number(req.query.n);
+        // alternates between domains
+        a = req.query.a;
+        b = req.query.b;
+        res.header("Set-Cookie", [`namefoo${n}=valfoo${n}`, `namebar${n}=valbar${n}`]);
+        if (n > 0) {
+          res.redirect(`${a}/setCascadingCookies?n=${n - 1}&a=${b}&b=${a}`);
+        }
+        return res.send("<html>finished setting cookies</html>");
       }
-      return res.send("<html>finished setting cookies</html>");
-    }
 
-    app.get('*', cascadingHandler);
+      app.get('*', cascadingHandler);
 
-    server = allowDestroy(app.listen(PORT))
+      server = allowDestroy(app.listen(PORT))
 
-    await page.goto('http://site1.foo.com/setCascadingCookies?n=8&a=http://site2.bar.net&b=http://site1.foo.com')
+      await page.goto(`http://site1.foo.com/setCascadingCookies?n=${N}&a=http://site2.bar.net&b=http://site1.foo.com`)
 
-    let cookies = await page.cookies('http://site1.foo.com', 'http://site2.bar.net')
-    cookies = _.reverse(_.sortBy(cookies, _.property('name')))
+      let cookies = await page.cookies('http://site1.foo.com', 'http://site2.bar.net')
+      cookies = _.reverse(_.sortBy(cookies, _.property('name')))
 
-    expect(cookies).toMatchSnapshot()
+      expect(cookies).toMatchSnapshot()
 
-    await browser.close()
-    await new Promise((r) => server.destroy(r))
+      await browser.close()
+      await new Promise((r) => server.destroy(r))
+    })
   })
 })
